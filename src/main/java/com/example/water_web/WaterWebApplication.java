bbd43e8d9c08d1da83f12212325fa0b5811b0060
정윤수 <https://github.com/whatpill1/whatpill.git>
2022-12-21 15:22:30 +0900

    Initial commit

---
+++ src/main/java/com/example/water_web/WaterWebApplication.java
@@ -0,0 +1,13 @@
+package com.example.water_web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WaterWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WaterWebApplication.class, args);
+    }
+
+}
